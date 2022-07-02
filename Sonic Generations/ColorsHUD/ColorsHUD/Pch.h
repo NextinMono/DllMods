dfc860abb7884e02092c983a1ea71838aaef031d
NextinMono <lorenzorai05@gmail.com>
2022-07-02 15:04:41 +0200

    Update Pch.h

--- Sonic Generations/ColorsHUD/ColorsHUD/Pch.h
+++ Sonic Generations/ColorsHUD/ColorsHUD/Pch.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#define WIN32_LEAN_AND_MEAN
+
 #include <BlueBlur.h>
 #include "INIReader.h"
 #include "ModLoader.h"
@@ -8,6 +10,10 @@
 #include "Common.h"
 #include "ScoreGenerationsAPI.h"
 
+#include <cstdint>
+#include <cstdio>
+#include <string>
+#include <filesystem>
 #include <memory>
 #include <windows.h>
 #include <winuser.h>
