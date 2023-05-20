480a909a03c9b897b261262697a5e35b70115e6e
NextinMono <lorenzorai05@gmail.com>
2023-05-20 22:01:39 +0200

    Begone thy secret mod!

--- Dependencies/Common.h
+++ Dependencies/Common.h
@@ -1080,6 +1080,35 @@ namespace Common
 		struct stat buffer;
 		return stat(file.c_str(), &buffer) == 0;
 	}
+	static bool fRaycast
+	(
+		Eigen::Vector4f const& rayStartPos,
+		Eigen::Vector4f const& rayEndPos,
+		Eigen::Vector4f& outPos,
+		Eigen::Vector4f& outNormal,
+		uint32_t flag
+	)
+	{
+		// sub_10BE3B0(&rayStartPos, flag, **(_DWORD ***)(*PLAYER_CONTEXT+ 1516), &outPos, &outNormal, &rayEndPos)
+		static void* const pfRaycast = (void*)0x10BE3B0;
+		__asm
+		{
+			mov		ecx, rayEndPos
+			push	ecx
+			mov		edx, outNormal
+			push	edx
+			mov		eax, outPos
+			push	eax
+			mov		ebx, PLAYER_CONTEXT
+			mov		ebx, [ebx]
+			mov     eax, [ebx + 5ECh]
+			mov     edi, [eax]
+			mov		edx, flag
+			mov		eax, rayStartPos
+			call[pfRaycast]
+		}
+	}
+
 	inline void GetModIniList(std::vector<std::string>& modIniList)
 	{
 		char buffer[MAX_PATH];
--- README.md
+++
@@ -1,9 +0,0 @@
-# NextinHKRY's DLL Mods
-Contains code injection mods' source for:
-- Sonic Generations
-- - Black Knight HUD 
-- - Ultimate Colors UI Port
-- - SWA Title Screen
-
-Feel free to take any code that you might need, and if you want to improve the code or make additions to it, feel free to do so!
-
